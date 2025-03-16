8e2de902f7404e7537c8be36b532dd21fd9dd287
oishioishi <hiroki.osawa.1215@outlook.jp>
2025-03-17 00:34:39 +0900

    Commit for Django-tutorial 7

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 class Question(models.Model):
@@ -10,6 +11,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     # def was_published_recently(self):
         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     # 修正後のwas_published_recentlyメソッド
